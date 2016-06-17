0acb59caaca6a4d3fc896b75e59645a72b86bd3a
u <ucha19871>
2016-06-17 20:48:35 +0400

    change

--- src/app/g-n2.component.ts
+++ src/app/g-n2.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from './task';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'g-n2-app',
   templateUrl: 'g-n2.component.html',
-  styleUrls: ['g-n2.component.css']
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/task', component: TaskComponent}
+])
 export class GN2AppComponent {
-  title = 'g-n2 works!';
+  title = 'Works!';
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -29,6 +29,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/task',
+  'app/+task',
   /** @cli-barrel */
 ];
 
