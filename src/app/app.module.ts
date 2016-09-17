b1aafbae29bd8e65721c5d49b3debca7a3844b2a
u <ucha19871>
2016-09-17 14:11:52 +0400

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search-box/search-box.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SearchBoxComponent } from './search-box.component';
+
+describe('Component: SearchBox', () => {
+  it('should create an instance', () => {
+    let component = new SearchBoxComponent();
+    expect(component).toBeTruthy();
+  });
+});
