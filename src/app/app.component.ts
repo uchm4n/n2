10b3790e8c63d536a71a884937521af15fbfcbde
HuchMan <ucha19871@gmail.com>
2016-09-18 15:18:45 +0400

    color sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,13 @@ import {Component} from '@angular/core';
     template: `
         <div>
             <h1>{{title}}</h1>
-            <search-box [placeHolder]="'Input Text to Clear'"></search-box>
             <hr>
-            <color-picker #picker color="red" (color)="onColor($event)"></color-picker>
-            <button class="btn btn-default center-block" (click)="picker.reset()">Reset</button>
+            <color-sample></color-sample>
         </div>`,
 })
 export class AppComponent {
 
     title = 'App is working Fine!';
-
-    onColor(color){
-        console.log(color);
-    }
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { SearchBoxComponent } from './search-box/search-box.component';
-import { ColorPickerComponent } from './color-picker/color-picker.component';
+import {AppComponent} from './app.component';
+import {SearchBoxComponent} from './search-box/search-box.component';
+import {ColorPickerComponent} from './color-picker/color-picker.component';
+import {ColorSampleComponent} from './color-sample/color-sample.component';
 
 @NgModule({
-  declarations: [AppComponent,SearchBoxComponent,ColorPickerComponent],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, SearchBoxComponent, ColorPickerComponent, ColorSampleComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
