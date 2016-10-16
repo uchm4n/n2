e2f7a26f27198239e799c1cb132c7a0d15521d89
u <ucha19871>
2016-10-16 21:01:49 +0400

    separated architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FormtestComponent } from './components/formtest/formtest.component';
 import { RatingModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FormtestComponent,
+    FooterComponent,
+    LogoComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
