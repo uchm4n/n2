c5c4265d0f4fa7dd4da50172029df59a7743f654
ucha <ucha@clips4sale.com>
2016-10-31 00:37:04 +0400

    implement canActive route Guard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,12 @@
 import {ModuleWithProviders} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 import {ContentComponent} from "./components/content/content.component";
+import {FormtestComponent} from "./components/formtest/formtest.component";
+import {AuthGuard} from "./auth.guard";
 
 const appRoutes: Routes = [
-  {path: '', component: ContentComponent},
+    {path: '', component: ContentComponent},
+    {path: 'formtest', component: FormtestComponent, canActivate: [AuthGuard]},
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,27 +1,16 @@
 import {AuthService} from "./services/auth.service";
-import {CanActivate, Router} from "@angular/router";
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
-
 
 
 @Injectable()
-export class AuthGuard {
-  constructor(private auth: AuthService, private router: Router) {}
-  
-  // canActivate(){
-  //   return console.log('')
-  //   // this.auth.auth$
-  //   // .take(1)
-  //   // .map(authState => !!authState)
-  //   // .do(authenticated => {
-  //   //   if (!authenticated) {
-  //   //     this.router.navigate(['/']);
-  //   //   }
-  //   // });
-  // }
+export class AuthGuard implements CanActivate{
+
+  constructor(private auth: AuthService){}
+
+  canActivate(route:ActivatedRouteSnapshot,state: RouterStateSnapshot):Observable<boolean> | boolean{
+    return this.auth.isAuthenticated();
+  }
   
 }
