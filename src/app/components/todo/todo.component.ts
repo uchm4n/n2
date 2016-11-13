ac4f06a9ca6a80d92086533f4849336ed8726bbe
ucha19871 <ucha19871@gmail.com>
2016-11-14 01:38:20 +0400

    implemented update needs little fix

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,24 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {TodoService} from "../../services/todo.service";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: 'todo.component.html',
-  styleUrls: ['todo.component.css'],
-  providers:[TodoService]
+    selector: 'app-todo',
+    templateUrl: 'todo.component.html',
+    styleUrls: ['todo.component.css'],
+    providers: [TodoService]
 })
 export class TODOComponent implements OnInit {
 
-  tasksList;
-  errorMessage;
-  constructor(private todo:TodoService) {}
+    tasksList;
+    errorMessage;
+
+    constructor(private todo: TodoService) {
+    }
 
 
-  ngOnInit() {
-    this.todo.getTasks().subscribe(
-        task => this.tasksList = task,
-        error =>  this.errorMessage = <any>error
-    );
-  }
+    ngOnInit() {
+        this.todo.getTasks().subscribe(
+            task => this.tasksList = task,
+            error => this.errorMessage = <any>error
+        );
+    }
+
+    toggleCompleted(task) {
+        let _todo = {
+            id: task.id,
+            title: task.title,
+            completed: !task.completed
+        }
+
+        this.todo.updateTask(_todo)
+            .subscribe(data => task.completed = !task.completed);
+
+        console.log(task)
+    }
 
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
 import {Http, Response, Headers, RequestOptions} from "@angular/http";
 import {TODO} from "../shared/todo";
 import {Observable} from "rxjs";
@@ -6,40 +6,49 @@ import {Observable} from "rxjs";
 @Injectable()
 export class TodoService {
 
-  private todoUrl = 'http://api.local/v1/task';  // URL to web API
-  private token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6XC9cL2FwaS5sb2NhbFwvdjFcL2F1dGgiLCJpYXQiOjE0NzkwNTM1NDEsImV4cCI6MTQ3OTA2NDM0MSwibmJmIjoxNDc5MDUzNTQxLCJqdGkiOiIzNjcxNDkzY2Q1ZGIzOGRmMjY3MzlmMzJkNjdhOTE4OCJ9.ljXj2QsjDoj_u8lfk9XYMaC1SkRU6P-GtNzauT3S54M';
-  constructor (private http: Http) {}
+    private todoUrl = 'http://api.local/v1/';  // URL to web API
+    private token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEwLCJpc3MiOiJodHRwOlwvXC9hcGkubG9jYWxcL3YxXC9hdXRoIiwiaWF0IjoxNDc5MDY4OTk3LCJleHAiOjE0NzkwODMzOTcsIm5iZiI6MTQ3OTA2ODk5NywianRpIjoiODYyMDI3ZjIwNjM1OTE4OTVkMTU3MmUxNDNjODliZTYifQ.Y26-UGIVjIEmMdwAyXXSDvLkuwDO6Vc69EsMyknlMmM';
+    private options;
 
-
-  getTasks(): Observable<TODO[]> {
-    let headers = new Headers({ 'Accept': 'application/json' });
-    headers.append('Authorization', `Bearer ${this.token}`);
-    let options = new RequestOptions({ headers: headers });
-
-    return this.http.get(this.todoUrl,options)
-        .map(this.extractData)
-        .catch(this.handleError);
-  }
-
-
-  private extractData(res: Response) {
-    let body = res.json();
-    return body || { };
-  }
-
-
-  private handleError (error: Response | any) {
-    // In a real world app, we might use a remote logging infrastructure
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
+    constructor(private http: Http) {
+        let headers = new Headers({'Accept': 'application/json'});
+        headers.append('Authorization', `Bearer ${this.token}`);
+        this.options = new RequestOptions({headers: headers});
     }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
-  }
+
+
+    getTasks(): Observable<TODO[]> {
+        return this.http.get(this.todoUrl + 'task', this.options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+
+    updateTask(task): Observable<TODO[]>  {
+        console.log(task)
+        return this.http.put(this.todoUrl + 'task/update/' + task.id, JSON.stringify(task), this.options)
+            .map(res => res.json());
+    }
+
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
+
+
+    private handleError(error: Response | any) {
+        // In a real world app, we might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 
 }
